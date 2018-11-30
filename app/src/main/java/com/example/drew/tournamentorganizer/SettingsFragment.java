3bd8030aff07abc278e2ecc229997d7aa24e56a7
Drew Williamson <>
2018-11-30 09:46:07 -0500

    added framing buttons for the Theme changing in Settings page

--- app/src/main/java/com/example/drew/tournamentorganizer/SettingsFragment.java
+++ app/src/main/java/com/example/drew/tournamentorganizer/SettingsFragment.java
@@ -1,5 +1,6 @@
 package com.example.drew.tournamentorganizer;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -8,6 +9,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.support.v4.app.Fragment;
+import android.widget.Button;
 import android.widget.TextView;
 
 /**
@@ -19,7 +21,22 @@ public class SettingsFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_settings, null);
+        View view = inflater.inflate(R.layout.fragment_settings, null);
+        Button darkButton = getActivity().findViewById(R.id.button_dark_theme);
+        Button lightButton = getActivity().findViewById(R.id.button_light_theme);
+//        darkButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//
+//            }
+//        });;
+//        lightButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//
+//            }
+//        });;
+        return view;
     }
 
     @Override
